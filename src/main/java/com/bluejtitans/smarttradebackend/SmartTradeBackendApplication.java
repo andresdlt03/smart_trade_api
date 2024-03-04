023f7e2e9e3f8c37a365926026d6b8d89693e1a5
andresdlt03 <andelatmo@gmail.com>
2024-03-04 07:14:35 +0100

    Initial commit

---
+++ src/main/java/com/bluejtitans/smarttradebackend/SmartTradeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bluejtitans.smarttradebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartTradeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartTradeBackendApplication.class, args);
+	}
+
+}
