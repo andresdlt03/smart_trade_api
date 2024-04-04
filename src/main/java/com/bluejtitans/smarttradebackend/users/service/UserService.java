2f501eb63708e81c8482fbebf71263c6f9140972
andresdlt03 <andelatmo@gmail.com>
2024-04-04 23:09:36 +0200

    improve register logic

--- src/main/java/com/bluejtitans/smarttradebackend/users/service/UserService.java
+++ src/main/java/com/bluejtitans/smarttradebackend/users/service/UserService.java
@@ -1,39 +1,47 @@
 package com.bluejtitans.smarttradebackend.users.service;
 
-import com.bluejtitans.smarttradebackend.users.model.Admin;
-import com.bluejtitans.smarttradebackend.users.model.Client;
-import com.bluejtitans.smarttradebackend.users.model.Seller;
+import com.bluejtitans.smarttradebackend.users.model.*;
 import com.bluejtitans.smarttradebackend.users.repository.AdminRepository;
 import com.bluejtitans.smarttradebackend.users.repository.ClientRepository;
 import com.bluejtitans.smarttradebackend.users.repository.SellerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-    private final AdminRepository adminRepository;
+
+private final AdminRepository adminRepository;
     private final ClientRepository clientRepository;
     private final SellerRepository sellerRepository;
 
     @Autowired
-    public UserService(AdminRepository adminRepository,
-                       ClientRepository clientRepository,
-                       SellerRepository sellerRepository) {
+    public UserService(AdminRepository adminRepository, ClientRepository clientRepository, SellerRepository sellerRepository) {
         this.adminRepository = adminRepository;
         this.clientRepository = clientRepository;
         this.sellerRepository = sellerRepository;
     }
 
-    public void saveClient(Client client) {
-        clientRepository.save(client);
+    public IUser registerUser(IUser user) {
+        CrudRepository<? extends User, String> repository = getRepository(user);
+
+        repository.findById(user.getEmail()).ifPresent(u -> {
+            throw new IllegalArgumentException("User already exists");
+        });
+
+        return user;
     }
 
-    public void saveSeller(Seller seller) {
-        sellerRepository.save(seller);
-    }
-
-    public void saveAdmin(Admin admin) {
-        adminRepository.save(admin);
+    private CrudRepository<? extends User, String> getRepository(IUser user) {
+        if (user instanceof Admin) {
+            return adminRepository;
+        } else if (user instanceof Client) {
+            return clientRepository;
+        } else if (user instanceof Seller) {
+            return sellerRepository;
+        } else {
+            throw new IllegalArgumentException("Invalid user type");
+        }
     }
 
 }
