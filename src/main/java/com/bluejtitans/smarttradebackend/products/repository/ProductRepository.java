9a8bb3aaa19d351224094890567d6c414d63bb0e
andresdlt03 <andelatmo@gmail.com>
2024-04-24 11:49:08 +0200

    improve productsservice

--- src/main/java/com/bluejtitans/smarttradebackend/products/repository/ProductRepository.java
+++ src/main/java/com/bluejtitans/smarttradebackend/products/repository/ProductRepository.java
@@ -3,6 +3,6 @@ package com.bluejtitans.smarttradebackend.products.repository;
 import com.bluejtitans.smarttradebackend.products.model.Product;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProductRepository extends CrudRepository<Product, Long> {
+public interface ProductRepository extends CrudRepository<Product, String> {
 
 }
