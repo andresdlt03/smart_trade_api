57dd0ef9190f63945c26a7f8d8fe348d49526f98
kililoti <fhchi2003@gmail.com>
2024-05-05 20:46:44 +0200

    Creado Excepciones.

---
+++ src/main/java/com/bluejtitans/smarttradebackend/exception/ProductNotFoundException.java
@@ -0,0 +1,7 @@
+package com.bluejtitans.smarttradebackend.exception;
+
+public class ProductNotFoundException extends Exception{
+    public ProductNotFoundException(String message){
+        super(message);
+    }
+}
