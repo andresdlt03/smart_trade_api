3f745ec104aaaeab7dd63c56c710ceeb779c34fb
andresdlt03 <andelatmo@gmail.com>
2024-05-07 09:41:02 +0200

    listas quitadas y servidor remoto

--- src/main/java/com/bluejtitans/smarttradebackend/lists/repository/GiftListRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bluejtitans.smarttradebackend.lists.repository;
-
-import com.bluejtitans.smarttradebackend.lists.model.PersonGift;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GiftListRepository extends CrudRepository<PersonGift, Long> {
-}
