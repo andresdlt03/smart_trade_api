0dfffe14f3751a51c389839cb069418d4b545bbc
kililoti <fhchi2003@gmail.com>
2024-05-21 23:55:34 +0200

    Para subir el repositorio

--- src/main/java/com/bluejtitans/smarttradebackend/lists/repository/ShoppingCartRepository.java
+++ src/main/java/com/bluejtitans/smarttradebackend/lists/repository/ShoppingCartRepository.java
@@ -3,7 +3,7 @@ package com.bluejtitans.smarttradebackend.lists.repository;
 import com.bluejtitans.smarttradebackend.lists.model.ShoppingCartProduct;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-//Esto es para pushear
+//Esto es para pushear.
 @Repository
 public interface ShoppingCartRepository extends CrudRepository<ShoppingCartProduct, Long> {
 }
