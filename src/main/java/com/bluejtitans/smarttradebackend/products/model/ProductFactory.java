2871dda8a32eed6547090d04df65be91941155ee
andresdlt03 <andelatmo@gmail.com>
2024-05-26 16:03:48 +0200

    restructure product factory

--- src/main/java/com/bluejtitans/smarttradebackend/products/model/ProductFactory.java
+++ src/main/java/com/bluejtitans/smarttradebackend/products/model/ProductFactory.java
@@ -8,13 +8,37 @@ public class ProductFactory {
     public static IProduct createProductFromDTO(String category, ProductDTO product) throws JsonProcessingException {
         return switch (category) {
             case "technology" ->
-                    new Technology(product.getName(), product.getDescription(), product.getDataSheet(), product.getPhotos(), product.getConsume(), product.getModel());
+                    new Technology(
+                            product.getName(),
+                            product.getDescription(),
+                            product.getDataSheet(),
+                            product.getPhotos(),
+                            product.getConsume(),
+                            product.getModel()
+                    );
             case "book" ->
-                    new Book(product.getName(), product.getDescription(), product.getDataSheet(), product.getPhotos(), product.getISBN());
+                    new Book(
+                            product.getName(),
+                            product.getDescription(),
+                            product.getDataSheet(),
+                            product.getPhotos(),
+                            product.getISBN());
             case "food" ->
-                    new Food(product.getName(), product.getDescription(), product.getDataSheet(), product.getPhotos(), product.getCalories());
+                    new Food(
+                            product.getName(),
+                            product.getDescription(),
+                            product.getDataSheet(),
+                            product.getPhotos(),
+                            product.getCalories()
+                    );
             case "clothes" ->
-                    new Clothes(product.getName(), product.getDescription(), product.getDataSheet(), product.getPhotos(), product.getSize());
+                    new Clothes(
+                            product.getName(),
+                            product.getDescription(),
+                            product.getDataSheet(),
+                            product.getPhotos(),
+                            product.getSize()
+                    );
             default -> throw new InvalidProductFormatException("Invalid product format") {
             };
         };
