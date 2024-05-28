a78721067f5c5d9ae22f38eb2c5f676a48af6a09
kililoti <fhchi2003@gmail.com>
2024-05-28 08:56:35 +0200

    Created Order exceptions.

---
+++ src/main/java/com/bluejtitans/smarttradebackend/exception/NotImplementedInCurrentStateException.java
@@ -0,0 +1,8 @@
+package com.bluejtitans.smarttradebackend.exception;
+
+public class NotImplementedInCurrentStateException extends Exception{
+    public NotImplementedInCurrentStateException(String message){
+        super(message);
+    }
+}
+
